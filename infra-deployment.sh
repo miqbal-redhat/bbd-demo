4a2fb3ec7c009743a2b188681b16c299e8f15835
miqbal-redhat <miqbal@redhat.com>
2021-05-03 11:33:18 -0700

    Update infra-deployment.sh

--- infra-deployment.sh
+++ infra-deployment.sh
@@ -1,9 +1,10 @@
 
+
 #!/bin/sh
 # This is a comment!
 echo Hello World        # This is a comment, too!
 
-Oc login 
+oc login 
 
 oc new-project poc-dev
 
@@ -12,4 +13,5 @@ oc process openshift//jenkins-ephemeral | oc apply -f- -n poc-dev
 oc set env dc/jenkins JENKINS_JAVA_OVERRIDES=-Dhudson.model.DirectoryBrowserSupport.CSP='' INSTALL_PLUGINS=ansicolor:0.5.2 -n poc-dev
 
 
-oc process -f applier/templates/infra-build.yaml  | oc apply -f- -n poc-dev
+oc process -f applier/templates/infra-build.yaml -p NAMESPACE=poc-dev| oc apply -f- -n poc-dev
+
